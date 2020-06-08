583f72f39b8dfe7e0874582d514bc31f1fb48521
diplomatic-chain-5587 <janiceviolin@gmail.com>
2020-06-08 22:04:31 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,2 +1,15 @@
-# Don't forget! This file needs to be 'required' in its spec file
-# See README.md for instructions on how to do this
+require_relative "../spec_helper.rb"
+
+def fizzbuzz(int)
+  if int %3 == 0
+    puts "Fizz"
+ end
+
+  if int %5 == 0
+    puts "Buzz"
+  end
+
+  if int %15 == 0
+    puts "FizzBuzz"
+  end
+end
